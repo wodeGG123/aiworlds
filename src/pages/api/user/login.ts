b9df992af3d24c958a87d210c9989ed518371cf3
Mac <mac@MacdeMacBook-Pro.local>
2025-06-06 16:40:31 +0800

    pag

--- src/pages/api/user/login.ts
+++ src/pages/api/user/login.ts
@@ -9,12 +9,16 @@ export default async function handler(
       url: "https://iablikcamuku.sealoshzh.site/api/v1/auth/login",
       method: "post",
       params: req.body,
+      headers: {
+        Cookie: "xxxxx",
+      },
     });
     // 透传响应头和状态码
     res.status(response.status);
     Object.entries(response.headers).forEach(([key, value]) => {
       res.setHeader(key, value);
     });
+
     res.json(response.data); // 关键：返回数据给客户端
   } catch (err) {
     console.log(err);
